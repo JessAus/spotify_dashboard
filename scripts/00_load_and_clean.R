89acf00831e995c7e92e153f649e75dfb73127a8
Jessie Ausman <jessaus1305@gmail.com>
2024-01-08 16:04:41 -0600

    more analysis of top artists/tracks

--- scripts/00_load_and_clean.R
+++ scripts/00_load_and_clean.R
@@ -4,9 +4,29 @@
 # Purpose: Load and Clean Spotify data                                              #
 #####################################################################################
 
-# Load req. packages
+# Load req. packages ----------------------------------------------------------------
 pacman::p_load(tidyverse, jsonlite, janitor)
+#-----------------------------------------------------------------------------------#
 
-# Read in data & clean column names
+
+# Read in data & clean column names -------------------------------------------------
 raw_stream <- fromJSON("data/StreamingHistory0.json", flatten=TRUE) %>%
   clean_names()
+#-----------------------------------------------------------------------------------#
+
+# Add columns for number of plays/time listened by artist/track (4 cols) ------------
+clean_stream <- raw_stream %>%
+  group_by(artist_name) %>%
+  mutate(plays_by_artist = n(),
+         time_listen_by_artist = sum(ms_played)) %>%
+  ungroup() %>%
+  group_by(track_name) %>%
+  mutate(plays_by_track = n(),
+         time_listen_by_track = sum(ms_played)) %>%
+  ungroup()
+#-----------------------------------------------------------------------------------#
+
+# Remove df/objects that are no longer needed ---------------------------------------
+rm(raw_stream)
+#-----------------------------------------------------------------------------------#
+
--- scripts/01_analysis.R
+++ scripts/01_analysis.R
@@ -8,41 +8,78 @@
 
 ## Create list of unique artists
 artists <- unique(raw_stream$artist_name)
-  ### 1090 unique artists in 2023 listening history ###
+  #===================================================#
+  #   1090 unique artists in 2023 listening history   #
+  #===================================================#
 
 ## Create list of unique tracks
 tracks <- unique(raw_stream$track_name)
-  ### 2814 unique tracks in 2023 listening history  ###
+  #===================================================#
+  #   2814 unique tracks in 2023 listening history    #
+  #===================================================#
 
 ## Total listening time 2023
 listening_ms_23 <- sum(raw_stream$ms_played)
-  ### 1,381,530,970 milliseconds listened ###
+  #===================================================#
+  #   1,381,530,970 milliseconds listened             #
+  #===================================================#
 
 listening_sec_23 <- listening_ms_23/1000
-  ### 1,381,530.97 seconds listened ###
+  #===================================================#
+  #     1,381,530.97 seconds listened                 #
+  #===================================================#
+
 
 listening_min_23 <- listening_sec_23/60
-  ### 23,025.516 minutes listened ###
+  #===================================================#
+  #     23,025.516 minutes listened                   #
+  #===================================================#
+
 
 listening_hr_23 <- listening_min_23/60
-  ### 383.759 hours listened  ###
+  #===================================================#
+  #     383.759 hours listened                        #
+  #===================================================#
 
 listening_days_23 <- listening_hr_23/24
-  ### 16 days ###
+  #===================================================#
+  #     16 days                                       #
+  #===================================================#
 
-## Number of plays by artist
-clean_stream <- raw_stream %>%
+## Is the 'top played artist' based on number of plays the same as 'top played artist' based on time played?
+top_played_by_num <- clean_stream %>%
+  distinct(by = artist_name, .keep_all = TRUE) %>%
   group_by(artist_name) %>%
-  mutate(plays_by_artist = n(),
-         time_listen_by_artist = sum(ms_played)) %>%
-  ungroup() %>%
-  group_by(track_name) %>%
-  mutate(plays_by_track = n(),
-         time_listen_by_track = sum(ms_played)) 
+  arrange(desc(plays_by_artist)) %>%
+  head(5) %>%
+  select(c("artist_name")) 
 
+top_played_by_time <- clean_stream %>%
+  distinct(artist_name, .keep_all = TRUE) %>%
+  group_by(artist_name) %>%
+  arrange(desc(time_listen_by_artist)) %>%
+  head(5) %>%
+  select(c("artist_name")) 
+  
+top_song_by_num <- clean_stream %>%
+  distinct(track_name, .keep_all = TRUE) %>%
+  group_by(track_name) %>%
+  arrange(desc(plays_by_track)) %>%
+  head(5) %>%
+  select(c("track_name"))
+
+top_song_by_time <- clean_stream %>%
+  distinct(track_name, .keep_all = TRUE) %>%
+  group_by(track_name) %>%
+  arrange(desc(time_listen_by_track)) %>%
+  head(5) %>%
+  select(c("track_name")) 
 #-----------------------------------------------------------------------------------#
 
 # Exploratory Analyses - By Month ---------------------------------------------------
+
+#-----------------------------------------------------------------------------------#
+
 
 # Exploratory Analyses - By Day of Week ---------------------------------------------
 
